df0aea32205570d25875df2cb2448af9704ce684
Jerva <Jerva@Jerva-iMac.local>
2015-02-08 00:32:44 +0200

    Tweaked the code and added 'help' keyword

--- isup_requests.py
+++ isup_requests.py
@@ -1,27 +1,23 @@
-import requests
+import requests, json
 
-keyword = raw_input('Enter keyword(ie. !up or !ups) : ')
+# Local testing area
+#keyword = raw_input('Enter keyword(ie. !up or !ups) : ')
+test = 'help'
+trigger_word = '!up'
 
-if keyword == '!up':
+if test == 'help':
+	#payload = {"text": "I look up given address and tell you if it's up or naw."}
+	print 'I look up given address and tell you if it\'s up or naw.'
 
-	test = raw_input('Enter url (ie. reddit.com): ')
+if trigger_word == '!up' and test != 'help':
+
 	out = 'http://www.'+test
 	conn = requests.get(out)
-	r1 = conn.status_code
+	status = conn.status_code
 
-	if r1 == 200:
-		print out + " is up. It's just you."
+	if  status == 200:
+		#payload = {"text": out + " is up. It's just you."}
+		print test + " is up. It's just you." #For local testing
 	else:
-		print out + " is down. Check back later"
-
-elif keyword == '!ups':
-
-	test = raw_input('Enter url (ie. reddit.com): ')
-	out = 'http://www.'+test
-	conn = requests.get(out)
-	r1 = conn.status_code
-
-	if r1 == 200:
-		print out + " is up. It's just you."
-	else:
-		print out + " is down. Check back later"
+		#payload = {"text": test + " is up. It's just you."}
+		print test + " is up. It's just you." #For local testing
