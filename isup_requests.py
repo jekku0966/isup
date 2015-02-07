ca4bd5bb21cc70df9783524ff833ea234021ec57
Jerva <Jerva@Jerva-iMac.local>
2015-02-08 01:08:51 +0200

    Test run

--- isup_requests.py
+++ isup_requests.py
@@ -2,22 +2,22 @@ import requests, json
 
 # Local testing area
 #keyword = raw_input('Enter keyword(ie. !up or !ups) : ')
-test = 'help'
+search = payload.json("text":"%s")
 trigger_word = '!up'
 
-if test == 'help':
-	#payload = {"text": "I look up given address and tell you if it's up or naw."}
-	print 'I look up given address and tell you if it\'s up or naw.'
+if search == 'help':
+	payload = {"text": "I look up given address and tell you if it's up or naw."}
+	#print 'I look up given address and tell you if it\'s up or naw.'
 
-if trigger_word == '!up' and test != 'help':
+if trigger_word == '!up' and search != 'help':
 
-	out = 'http://www.'+test
+	out = 'http://www.'+search
 	conn = requests.get(out)
 	status = conn.status_code
 
 	if  status == 200:
-		#payload = {"text": out + " is up. It's just you."}
-		print test + " is up. It's just you." #For local testing
+		payload = {"text": search + " is up. It's just you."}
+		#print search + " is up. It's just you." #For local testing
 	else:
-		#payload = {"text": test + " is up. It's just you."}
-		print test + " is up. It's just you." #For local testing
+		payload = {"text": search + " is up. It's just you."}
+		#print search + " is up. It's just you." #For local testing
