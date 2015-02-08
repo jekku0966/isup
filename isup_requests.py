264a33bc1cd37e219c6912a3def4f5111c7db0f7
Jerva <Jerva@Jerva-iMac.local>
2015-02-09 00:13:01 +0200

    no message

--- isup_requests.py
+++ isup_requests.py
@@ -1,54 +1,46 @@
-import requests, json
+import requests
 from slacker import Slacker
-from flask import  Flask, request
-
-text = '!up reddit.com'
 
 slack = Slacker('xoxp-3478131362-3477154371-3505081635-51a8a0')
 username = 'upbot'
-icon_emoji = 'mag'
+icon_emoji = ':mag:'
 commands = ['up']
 
 
+
 def post_message(text):
-	slack.chat.post_message(text = text,
-													username = username,
-													icon_emoji = icon_emoji)
+	slack.chat.post_message(text = text,	username = username, icon_emoji = icon_emoji)
 
-def need_help():
-	post_message('I look up given address and tell you if it\'s up or naw with command !up \%URL%.')
 
-def respond():
-	search = text - command
+def respond(text):
+	search = text
 	out = 'http://www.'+search
+	outs = 'https://www.'+search
 	conn = requests.get(out)
+	conn1 = requests.get(outs)
 	status = conn.status_code
+	status1 = conn1.status_code
 
-	if  status == 200:
-		#slack.chat.post_message(search + ' is up. It\'s just you.')
-		print search + ' is up. It\'s just you.'
+	if status == 200 and status1 == 200:
+		post_message(search + ' is up. It\'s just you.')
+		#print search + ' is up. It\'s just you.'
 	else:
-		#slack.chat.post_message(search + 'is down. Check back later.')
-		print search + 'is down. Check back later.'
+		post_message(search + ' is down. Check back later.')
+		#print search + ' is down. Check back later.'
+
 
 def main():
 	text = request.form.get("text", "")
-	match = re.findall(r"!(\S+)", text)
 
+	match = re.findall(r"!(\S+)", text)
 	if not match:
 		return
 
 		command = match[0]
-   	args = text.replace("!%s" % command, '')
-   	command = command.lower()
-   
-	if command not in commands:
-		post_message('Not sure what "%s" is.' % command)
-		return json.dumps({ })
+  	args = text.replace("!%s" % command, '')
+  	command = command.lower()
 
-	if command == 'up':
-		respond()
-	if command == 'up' and 'help':
-		need_help()
+  	if command == '!up':
+  		respond()
 
-	return json.dumps({ })
+    	return json.dumps({ })	
