317be27555a63a0215ee459ec410a39ec8e8c460
Jerva <Jerva@Jerva-iMac.local>
2015-02-09 01:15:34 +0200

    no message

--- isup_requests.py
+++ isup_requests.py
@@ -4,7 +4,8 @@ from slacker import Slacker
 slack = Slacker('xoxp-3478131362-3477154371-3505081635-51a8a0')
 username = 'upbot'
 icon_emoji = ':mag:'
-commands = ['up']
+keyword = '!up'
+text = ''
 
 
 
@@ -12,8 +13,8 @@ def post_message(text):
 	slack.chat.post_message(text = text,	username = username, icon_emoji = icon_emoji)
 
 
-def respond(text):
-	search = text
+if keyword == '!up':
+	search = text[3:]
 	out = 'http://www.'+search
 	outs = 'https://www.'+search
 	conn = requests.get(out)
@@ -21,26 +22,9 @@ def respond(text):
 	status = conn.status_code
 	status1 = conn1.status_code
 
-	if status == 200 and status1 == 200:
+	if status == 200 or status1 == 200:
 		post_message(search + ' is up. It\'s just you.')
 		#print search + ' is up. It\'s just you.'
 	else:
 		post_message(search + ' is down. Check back later.')
 		#print search + ' is down. Check back later.'
-
-
-def main():
-	text = request.form.get("text", "")
-
-	match = re.findall(r"!(\S+)", text)
-	if not match:
-		return
-
-		command = match[0]
-  	args = text.replace("!%s" % command, '')
-  	command = command.lower()
-
-  	if command == '!up':
-  		respond()
-
-    	return json.dumps({ })	
