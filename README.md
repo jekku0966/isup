77e8da0292cf34872deee994270793723e330251
jekku0966 <Jerva@Jerva-iMac.local>
2015-02-14 11:53:41 +0200

    Had to take out IP based status resolving. Was timing out. Need to find a way to do this.

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 [![Codacy Badge](https://www.codacy.com/project/badge/2a784e717c714e71b809a1b4bcf5d3fa)](https://www.codacy.com/public/app33805610/isup)
 
-A tool to check whether a given url is up or not.
+A tool to check whether a given url is up or not from Slack.
 
-I have no previous python coding experience except codecademy.com python course. I think I did a pretty good job on doing this.
+I have no previous python coding experience except codecademy.com python course which I haven't even finished. I think I did a pretty good job on doing this.
 
 And we have a working copy!
 
@@ -13,5 +13,5 @@ Thanks to /u/m3lkor001 at /r/learnpython for helping me to solve the parsing iss
 
 ### TODO:
 * Streamline the code for faster response (duh)
-* ????
-* Profit
+* Need to find a way to resolve IP addresses
+* Non-Profit
--- isup.py
+++ isup.py
@@ -1,58 +1,62 @@
+# Import needed modules
 import cherrypy
 import json
 import requests
 
 class IsUp(object):
     exposed = True
-
-    # Make cherrypy accept content type
-    #@cherrypy.tools.accept(media='*/*')
   
     def POST(self, **kwargs):
+      # Uncomment console output
+      print cherrypy.request.body.params
 
-       # Uncomment console output
-       print cherrypy.request.body.params
+      # Parse received url encoded body
+      json_parse = json.dumps(cherrypy.request.body.params)
+      # Decode the json
+      decoded = json.loads(json_parse)
+      print decoded
+      # Transfer decoded json from {"text": "!up <http://google.com|google.com>"} to trigger
+      trigger = decoded['text']
+      # Select only the first 3 from the trigger
+      keyword = trigger[:3]
+      # Split !up <http://google.com|google.com> to 2 different with from | and select the latter and remove '>'
+      url = trigger.split('|')[1][:-1]
 
-       # Parse received json
-       json_parse = json.dumps(cherrypy.request.body.params)
-       # Decode the json
-       decoded = json.loads(json_parse)
-       print decoded
-       # Transfer decoded json from {"text": "!up <http://google.com|google.com>"} to trigger
-       trigger = decoded['text']
-       # Select only the first 3 from the trigger
-       keyword = trigger[:3]
-       # Split !up <http://google.com|google.com> to 2 different with from | and select the latter and remove '>'
-       url = trigger.split('|')[1][:-1]
+      # Uncomment for console output of keyword and url
+      print keyword
+      print url
 
-       # Uncomment for console output of keyword and url
-       print keyword
-       print url
-
-       if keyword == '!up':
+      #Self explanatory
+      if keyword == '!up':
         try:
           out = 'http://'+url
           conn = requests.get(out)
           status = conn.status_code
-        except Exception:
+        # If any exception is occurred try the next one
+        except Exception as e:
+          print e
           try:
             out1 = 'http://www'+url
             conn1 = requests.get(out1)
             status1 = conn1.status_code
-          except Exception:
+          except Exception as e:
+            print e
             try:
               out2 = 'https://www'+url
               conn2 = requests.get(out2)
               status2 = conn2.status_code
-            except Exception:
-              return json.dumps({'text': 'Given url doesn\'t exist.'})
+            except Exception as e:
+              print e
+              # Finally return an error if URI can't be resolved
+              return json.dumps({'text': 'Given url doesn\'t exist or I can\'t resolve it. Please check the validity of the URI.'})
 
+        # Return the answer. Either it's up or down.
         if status == 200 or status1 == 200 or status2 == 200:
           return json.dumps({'text': '%s is up. It\'s just you.' % url})
         else:
           return json.dumps({'text':'%s is down. Check back later.' % url})
 
-        
+# Start the Cherypy server        
 if __name__ == '__main__':
     cherrypy.config.update("server.conf")
     cherrypy.quickstart(IsUp(), '/', "app.conf")
